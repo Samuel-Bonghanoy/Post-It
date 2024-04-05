2e416c356578ca45d92a8ee4b49952e072c56647
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-04-05 12:59:26 +0800

    refactor: change theme colors

--- src/App.vue
+++ src/App.vue
@@ -3,48 +3,13 @@ import Navbar from "./components/Navigation/Navbar.vue";
 </script>
 
 <template>
-  <section
-    class="flex flex-col w-screen h-screen gap-8 p-10 bg-white dark:bg-gray-800 rounded-xl"
-  >
-    <navbar />
-    <h1 class="text-4xl font-bold text-center text-black dark:text-white">
-      Tailwind CSS + PrimeVue
-    </h1>
-    <Panel header="Default Preset">
-      <p>
-        First panel component uses the global pass through preset from the
-        Tailwind CSS based implementation of PrimeOne Design 2023.
-      </p>
-    </Panel>
-
-    <Panel
-      header="Custom Header"
-      :pt="{
-        header:
-          'p-5 flex items-center justify-between border border-indigo-300 bg-indigo-500 text-indigo-50 rounded-tl-lg rounded-tr-lg dark:bg-indigo-900 dark:border-indigo-900/40 dark:text-white/80',
-      }"
-    >
-      <p>Second panel overrides the header section with custom style.</p>
-    </Panel>
-
-    <Panel
-      header="Custom Design"
-      :ptOptions="{ mergeSections: false }"
-      :pt="{
-        header:
-          'flex items-center justify-center p-5 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 rounded-tl-2xl rounded-tr-2xl text-white',
-        title: 'leading-none font-bold uppercase text-2xl',
-        content:
-          'bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 px-5 pb-8 pt-3 text-white text-center rounded-bl-2xl rounded-br-2xl text-xl',
-      }"
-    >
-      <p>
-        Third panel ignores the default preset with
-        <b>mergeSections: false</b> and applies a custom style to all elements
-        of the panel instead.
-      </p>
-    </Panel>
-  </section>
+  <div class="w-screen h-screen bg-midnight">
+    <Navbar />
+    <div class="flex flex-col w-6/12 pt-10 mx-auto font-sans bg-midnight">
+      <h1 class="pb-3 text-3xl font-black text-slate-100"></h1>
+      <!-- <router-view /> -->
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
