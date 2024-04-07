660ff1904f4e48fccc8798d087b7220b145abd81
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-04-07 14:40:15 +0800

    feat: edit sidebar

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Navbar from "./components/Navigation/Navbar.vue";
   <div class="w-screen h-screen overflow-x-hidden bg-primary-50">
     <Navbar />
     <section class="flex w-full h-full">
-      <div class="w-[25%] relative">
+      <div class="w-[25%] sticky">
         <div
           class="w-[0.3px] h-full absolute top-[5%] left-[90%] bg-primary-200"
         ></div>
