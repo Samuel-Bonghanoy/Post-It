b72160d1ac0aeb29b6baa13c4afb9df958869463
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-04-07 15:01:59 +0800

    feat: create sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import Navbar from "./components/Navigation/Navbar.vue";
+import Sidebar from "./components/Navigation/Sidebar.vue";
 </script>
 
 <template>
   <div class="w-screen h-screen overflow-x-hidden bg-primary-50">
     <Navbar />
     <section class="flex w-full h-full">
-      <div class="w-[25%] sticky">
+      <div class="w-[20%] sticky flex">
+        <Sidebar />
         <div
           class="w-[0.3px] h-full absolute top-[5%] left-[90%] bg-primary-200"
         ></div>
       </div>
 
-      <div class="flex flex-col w-[75%] pt-10 mx-auto font-sans bg-midnight">
+      <div class="flex flex-col w-[80%] pt-10 mx-auto font-sans bg-midnight">
         <h1 class="pb-3 text-4xl font-bold text-slate-100">
           Welcome Back, Amy
         </h1>
