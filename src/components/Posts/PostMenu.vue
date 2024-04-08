cb06f9aa45b160a1d2b2788a096e3a58b902563b
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-04-08 19:36:21 +0800

    feat: make like button functionality

--- src/components/Posts/PostMenu.vue
+++ src/components/Posts/PostMenu.vue
@@ -26,8 +26,8 @@ const onClick = () => {
       xmlns="http://www.w3.org/2000/svg"
       width="25"
       height="25"
-      class="transition-all duration-75 hover:fill-primary-300 fill-[#352f44] hover:cursor-pointer"
-      :class="{ 'fill-primary-300': isActive }"
+      class="transition-all duration-75 hover:fill-primary-300 hover:cursor-pointer"
+      :class="[isActive ? 'fill-primary-300' : 'fill-primary-100']"
       @click="onClick"
       viewBox="0 0 256 256"
     >
@@ -47,6 +47,5 @@ const onClick = () => {
         d="M229.66,109.66l-48,48A8,8,0,0,1,168,152V112h-3a88,88,0,0,0-85.23,66,8,8,0,0,1-15.5-4A103.94,103.94,0,0,1,165,96h3V56a8,8,0,0,1,13.66-5.66l48,48A8,8,0,0,1,229.66,109.66ZM192,208H40V88a8,8,0,0,0-16,0V216a8,8,0,0,0,8,8H192a8,8,0,0,0,0-16Z"
       ></path>
     </svg>
-    <p>{{ isActive }}</p>
   </div>
 </template>
