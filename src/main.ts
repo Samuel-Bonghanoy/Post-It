79bf592a70e45122285c048d9dc965a7074f98db
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-04-09 11:44:23 +0800

    chore: install pinia

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import "./index.css";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import Lara from "./presets/lara";
-import Panel from "primevue/panel";
+import { createPinia } from "pinia";
 import router from "./router";
 
+const pinia = createPinia();
 const app = createApp(App);
 
 app.use(PrimeVue, { unstyled: true, pt: Lara });
 app.use(router);
-
-app.component("Panel", Panel);
+app.use(pinia);
 
 app.mount("#app");
