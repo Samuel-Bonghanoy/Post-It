bcc8eb514bc9f9465bdc194b9b2197733dc81690
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-04-09 20:30:18 +0800

    feat: create state for form input

--- src/components/Inputs/PostInputModal.vue
+++ src/components/Inputs/PostInputModal.vue
@@ -2,9 +2,12 @@
 import Dialog from "primevue/dialog";
 import Button from "primevue/button";
 import Textarea from "./Textarea.vue";
+import InputText from "primevue/inputtext";
 import { ref } from "vue";
 
 const visible = ref(false);
+const title = ref("");
+const body = ref("");
 </script>
 
 <template>
@@ -24,11 +27,15 @@ const visible = ref(false);
       :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
       class="flex flex-col"
     >
-      <div class="flex flex-col gap-5">
-        <Textarea />
+      <div class="flex flex-col gap-2">
+        <h3 class="font-semibold">Post Title</h3>
+        <InputText v-model="title" />
+
+        <h3 class="font-semibold">Post Content</h3>
+        <Textarea v-model="body" />
         <Button
           label="Write Post"
-          class="self-end w-[20%]"
+          class="self-end w-[20%] mt-2"
           @click="visible = false"
           severity="success"
         />
