6d8ae3e8b98c2f0cffff66f2bd492143237863da
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-04-10 12:29:11 +0800

    style: edit colors on signup page

--- src/components/SignIn/SignIn.vue
+++ src/components/SignIn/SignIn.vue
@@ -16,14 +16,37 @@
     >
       <div class="text-center">
         <span
-          class="inline-flex items-center justify-center p-4 rounded-full bg-white/10"
+          class="flex items-center justify-center p-4 rounded-full bg-white/10"
         >
-          <i class="text-4xl text-white pi pi-user" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="48"
+            height="48"
+            viewBox="0 0 256 256"
+            class="mr-3 fill-primary-50"
+          >
+            <path d="M192,80v88H120v32H64V80Z" opacity="0.2"></path>
+            <path
+              d="M240,192h-8V56a16,16,0,0,0-16-16H40A16,16,0,0,0,24,56V192H16a8,8,0,0,0,0,16H240a8,8,0,0,0,0-16ZM40,56H216V192H200V168a8,8,0,0,0-8-8H120a8,8,0,0,0-8,8v24H72V88H184v48a8,8,0,0,0,16,0V80a8,8,0,0,0-8-8H64a8,8,0,0,0-8,8V192H40ZM184,192H128V176h56Z"
+            ></path>
+          </svg>
         </span>
       </div>
-      <InputText placeholder="Username" />
-      <InputText placeholder="Password" />
-      <Button label="Login" />
+
+      <input
+        class="justify-center p-2 font-semibold rounded-lg bg-white/10 text-primary-50 placeholder-primary-50"
+        placeholder="Username"
+      />
+      <input
+        class="justify-center p-2 font-semibold rounded-lg placeholder-primary-50 bg-white/10 text-primary-50"
+        placeholder="Password"
+      />
+
+      <button
+        class="justify-center p-2 font-semibold rounded-lg bg-white/10 text-primary-50 placeholder-primary-50"
+      >
+        Log in
+      </button>
     </div>
   </div>
 </template>
